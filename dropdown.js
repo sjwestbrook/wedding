4d32fb6eca19666f6f72ecd59bf9f524b84e1c80
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-08-01 08:11:48 -0600

    Create dropdown.js

---
+++ dropdown.js
@@ -0,0 +1,19 @@
+// DROPDOWN MENU //
+var dropDown = function () {
+  document.getElementById("myDropdown").classList.toggle("show");
+};
+
+window.onclick = function (event) {
+  if (!event.target.matches('.fa-bars')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+// DROPDOWN MENU //
